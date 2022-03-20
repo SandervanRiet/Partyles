456cbb250d344fff3f8b37c538caf013832230df
Sander van Riet <r0903044@student.thomasmore.be>
2022-03-20 15:08:43 +0100

    complex queries afgewerkt

---
+++ src/main/java/be/thomasmore/party/repositories/AnimalRepository.java
@@ -0,0 +1,14 @@
+package be.thomasmore.party.repositories;
+
+import be.thomasmore.party.model.Animal;
+import be.thomasmore.party.model.Artist;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface AnimalRepository extends CrudRepository<Animal, Integer> {
+    Optional<Animal> findFirstByIdLessThanOrderByIdDesc(int id);
+    Optional<Animal> findFirstByIdGreaterThanOrderById(int id);
+    Optional<Animal> findFirstByOrderByIdDesc();
+    Optional<Animal> findFirstByOrderByIdAsc();
+}
